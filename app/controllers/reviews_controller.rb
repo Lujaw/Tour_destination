b5092c85bb264e898a5c761e7baa5643cdb26079
Lujaw Shrestha <lujaw@hotmail.com>
2011-12-12 14:25:10 +0545

    Ajax implemented

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,24 @@
 class ReviewsController < ApplicationController
-
-
   before_filter :find_destination
-
-  def create
-
-    @review = @destination.reviews.create(params[:review])
-
-    redirect_to destination_path(@destination)
-
-    # render :text => "Param: #{params}"
-
+  def create  
+  #  if @destination.user_id!=current_user.id
+   if @destination.user != current_user
+       @review = current_user.reviews.create(params[:review])
+       @review.destination_id = @destination.id
+      
+      respond_to do |format|  
+        if @review.save  
+          format.html { redirect_to destination_path(@destination) }  
+          format.js    
+        end  
+      end  
+    else 
+       redirect_to destinations_path
+        flash[:alert] = "You can't comment at your own post"
+    end
   end
-
-
   private
-
   def find_destination
     @destination = Destination.find(params[:destination_id])
   end
-
 end
